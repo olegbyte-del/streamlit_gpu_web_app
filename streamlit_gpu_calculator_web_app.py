80939e13def2f8ff9ffe033de3fa609d66a0505f
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-03 09:27:46 +0800

    Added DataFrame Operations with pandas along with random geneed DataFrame

--- streamlit_gpu_calculator_web_app.py
+++ streamlit_gpu_calculator_web_app.py
@@ -15,7 +15,7 @@ st.title("Streamlit App with GPU Computations")
 st.sidebar.header("Choose an Operation")
 operation = st.sidebar.selectbox("Select a computation:",
                                 ["Matrix Multiplication (Numpy)", "DataFrame Operations (Pandas)",
-                                "Tensor Computtaions (Pytorch)", "Tran and Test a Deep Learning Model"])
+                                "Tensor Computations (Pytorch)", "Tran and Test a Deep Learning Model"])
 
 # Main app Functionality
 if operation == "Matrix Multiplication (Numpy)":
@@ -25,3 +25,35 @@ if operation == "Matrix Multiplication (Numpy)":
     rows = st.number_input("Number of rows:", min_value = 1, max_value =  1000, value = 3)
     cols = st.number_input("Number of columns", min_value = 1, max_value= 1000, value = 3)
     
+    if st.button("Generate and Multiply Matrices"):
+        # Generate random matrices
+        matrix_a = np.random.rand(rows, cols)
+        matrix_b = np.random.rand(cols, rows)
+        
+        # Perform matrix multiplication
+        result = np.dot(matrix_a, matrix_b)
+        
+        st.write("Matrix A:")
+        st.write(matrix_a)
+        
+        st.write("Matrix B:")
+        st.wrtie(matrix_b)
+        
+        st.write("Resultant Matrix:")
+        st.write(result)
+        
+elif operation == "DataFrme Operations (Pandas)":
+    st.header("DataFrame Operations with Pandas")
+    
+    # Generate a random DataFrame
+    rows = st.number_input("Number of rows:", min_value = 0, max_value = 1000, value = 10)
+    
+    if st.button("Generate DataFrame"):
+        df = pd.DataFrame(np.random.rand(rows, 5))
+        columns= ["A", "B", "C", "D", "E"]
+
+        st.write("Randomly Generated DataFrame:")
+        st.write(df)
+        
+        st.write("Column Summaries:")
+        st.write(df.describe())

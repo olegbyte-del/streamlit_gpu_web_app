1f16aaaabf42468955af52210086a5068d7c6dd9
olegbyte-del <angeloferrer5436@gmail.com>
2026-03-03 23:45:55 +0800

    Fixed mispelling on comment dimensionss --> dimension

--- streamlit_gpu_calculator_web_app.py
+++ streamlit_gpu_calculator_web_app.py
@@ -16,7 +16,7 @@ operation = st.sidebar.selectbox("Select a computation:",
 if operation == "Matrix Multiplication (Numpy)":
     st.header("Matrix Multiplication with Numpy")
     
-    # User inputs for matrix dimensionss
+    # User inputs for matrix dimensions
     rows = st.number_input("Number of rows:", min_value=1, max_value=1000, value=3)
     cols = st.number_input("Number of columns:", min_value=1, max_value=1000, value=3)
     
